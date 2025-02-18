459b20ae8447317c0ee42118f0852bf1ff33c8ad
朱波 <956903326@qq.com>
2025-02-18 10:44:17 +0800

    feat: router

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,15 +3,37 @@ import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 
 // 配置路由
-const routes: Array<RouteRecordRaw> = [
-    {
-        path: "/",
-        name: "home",
-        component: () => import("../views//home/index.vue"),
-        meta: {},
-        children: [],
-    },
-];
+// const routes: Array<RouteRecordRaw> = [
+//     {
+//         path: "/",
+//         name: "home",
+//         component: () => import("../views//home/index.vue"),
+//         meta: {},
+//         children: [],
+//     },
+// ];
+
+export const aboutRouter = {
+    path: '/about',
+    name: 'about',
+    component: () => import('@/views/about/index.vue'),
+    meta: {},
+    children: [],
+} as RouteRecordRaw;
+
+// 组合路由信息
+// import.meta.glob 为 vite 提供的特殊导入方式
+// 它可以将模块中全部内容导入并返回一个Record对象
+// 默认为懒加载模式 加入配置项 eager 取消懒加载
+const modules: Record<string, any> = import.meta.glob(["./modules/*.ts"], {
+    eager: true,
+});
+const routes: Array<RouteRecordRaw> = [];
+Object.keys(modules).forEach((key) => {
+    const module = modules[key].default;
+    routes.push(module);
+});
+routes.push(aboutRouter);
 
 const router = createRouter({
     history: createWebHashHistory(),
---
+++ src/views/login/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>login</div>
+</template>
+<script lang="ts" setup>
+// import { useUserStore } from '@/store/user';
+// const userStore = useUserStore();
+// console.log(userStore);
+</script>
+<style lang="less" scoped></style>
